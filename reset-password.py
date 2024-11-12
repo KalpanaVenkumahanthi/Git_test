09c41ca0f7ca3c4a1f4bb8c466ec771c6bbaefc0
Kalpana Venkumahanthi <rajkalpana06@gmail.com>
2024-11-12 15:38:58 +0530

    added reset-password

---
+++ reset-password.py
@@ -0,0 +1,6 @@
+user_name = input("enter user name =")
+password = input("enter user password =")
+if (user_name=="Admin" and password=="Admin"):
+    print("you can reset password")
+else:
+    print("you cannot reset")
